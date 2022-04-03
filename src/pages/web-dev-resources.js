cce826b70637f19c70d016135a392cbae51052d4
dkersten <dkersten1994@gmail.com>
2022-04-03 18:10:48 -0400

    finished v1 of web resources

--- src/pages/web-dev-resources.js
+++ src/pages/web-dev-resources.js
@@ -39,11 +39,116 @@ const WebDevResources = () => {
                     }
                 }
             }
+            designResources: allContentfulDevResources(filter: {resourceCategory:{eq: "design"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            fontResources: allContentfulDevResources(filter: {resourceCategory:{eq: "fonts"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            imageResources: allContentfulDevResources(filter: {resourceCategory:{eq: "img"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            inspirationResources: allContentfulDevResources(filter: {resourceCategory:{eq: "inspiration"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            learningResources: allContentfulDevResources(filter: {resourceCategory:{eq: "learning"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            miscellaneousResources: allContentfulDevResources(filter: {resourceCategory:{eq: "misc"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
+            toolingResources: allContentfulDevResources(filter: {resourceCategory:{eq: "tooling"}}) {
+                edges {
+                    node {
+                        resourceName
+                        resourceUrl
+                        resourceAbbreviation
+                        resourceCategory
+                        resourceDescription {
+                            resourceDescription
+                        }
+                        id
+                    }
+                }
+            }
         }
     `)
     console.log(data)
     const accessibilityData = data.accessibilityResources.edges
     const colorData = data.colorResources.edges
+    const designData = data.designResources.edges
+    const fontsData = data.fontResources.edges
+    const imagesData = data.imageResources.edges
+    const inspirationData = data.inspirationResources.edges
+    const learningData = data.learningResources.edges
+    const miscellaneousData = data.miscellaneousResources.edges
+    const toolingData = data.toolingResources.edges
 
     return(
         <Layout>
@@ -76,6 +181,111 @@ const WebDevResources = () => {
                             <div className="flex-container">
                                 {
                                     colorData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Design</h2>
+                            <div className="flex-container">
+                                {
+                                    designData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Fonts</h2>
+                            <div className="flex-container">
+                                {
+                                    fontsData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Images</h2>
+                            <div className="flex-container">
+                                {
+                                    imagesData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Inspiration</h2>
+                            <div className="flex-container">
+                                {
+                                    inspirationData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Learning</h2>
+                            <div className="flex-container">
+                                {
+                                    learningData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Miscellaneous</h2>
+                            <div className="flex-container">
+                                {
+                                    miscellaneousData.map(resource =>
+                                        <ResourceCard
+                                            key={resource.node.id}
+                                            {...resource.node}
+                                        />
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </section>
+                    <section className="resource-section">
+                        <div className="page-width">
+                            <h2>Tooling</h2>
+                            <div className="flex-container">
+                                {
+                                    toolingData.map(resource =>
                                         <ResourceCard
                                             key={resource.node.id}
                                             {...resource.node}
